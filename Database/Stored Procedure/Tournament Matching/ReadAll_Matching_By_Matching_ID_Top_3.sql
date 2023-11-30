393b7964a0166e8e7d25fe9469e219e9ddf8689e
Rian Dave Brignas <brignasriandave@gmail.com>
2023-12-01 01:32:46 +0800

    Updated - ReadAll_Matching_By_Matching_ID_Top_3.sql

--- Database/Stored Procedure/Tournament Matching/ReadAll_Matching_By_Matching_ID_Top_3.sql
+++ Database/Stored Procedure/Tournament Matching/ReadAll_Matching_By_Matching_ID_Top_3.sql
@@ -1,9 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[ReadAll_Matching_By_Matching_ID_Top_3]
-	@ID varchar(50)
+    @ID VARCHAR(50)
 AS
-	SELECT TOP 3 * 
-	FROM dbo.Tournament_Matching
-	INNER JOIN dbo.Tournament_Matching_Bracket
-	ON Tournament_Matching_Bracket_ID = Matching_Bracket_ID
-	WHERE Matching_Bracket_ID = @ID AND Tournament_Matching_Is_Done = 0
-	ORDER BY Tournament_Matching_Sort_Number ASC
+    SELECT *
+    FROM (
+        SELECT *, ROW_NUMBER() OVER (ORDER BY Tournament_Matching_Sort_Number ASC) AS RowNum
+        FROM dbo.Tournament_Matching
+        INNER JOIN dbo.Tournament_Matching_Bracket
+            ON Tournament_Matching_Bracket_ID = Matching_Bracket_ID
+        WHERE Matching_Bracket_ID = @ID AND Tournament_Matching_Is_Done = 0
+    ) AS NumberedRows
+    WHERE RowNum BETWEEN 2 AND 4;
