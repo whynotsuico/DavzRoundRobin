21954bbf4e7c9a686e8a3101b9e53ef1c9c9cc0d
Rian Dave Brignas <brignasriandave@gmail.com>
2023-12-03 17:39:00 +0800

    Added Default Value

--- Database/Tables/Tournament_Registration.sql
+++ Database/Tables/Tournament_Registration.sql
@@ -6,5 +6,5 @@
     [Tournament_Registration_Drag_Bike_Number] NVARCHAR(100) NULL, 
     [Tournament_Registration_Category_ID] INT NOT NULL, 
     [Tournament_Registration_Team_Name] NVARCHAR(250) NULL, 
-    [Tournament_Registration_Is_Already_Bracket] BIT NULL
+    [Tournament_Registration_Is_Already_Bracket] BIT NULL DEFAULT 0
 )
