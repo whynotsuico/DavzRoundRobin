aa485a4af056e2b2025b10d93c262bafbee83449
Wilrey Suico <suicowhynot@gmail.com>
2024-04-22 20:27:37 +0800

    fixed

--- Database/Stored Procedure/Tournament Category/ReadAll_Category_Not_In_Event.sql
+++ Database/Stored Procedure/Tournament Category/ReadAll_Category_Not_In_Event.sql
@@ -1,5 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[ReadAll_Category_Not_In_Event]
+ @EventID INT
+
 AS
 	SELECT * FROM dbo.Tournament_Category
 	WHERE Tournament_Category_ID
-	NOT IN (SELECT Registration_Category_Category_ID FROM Tournament_Registration_Category)
+	NOT IN (SELECT Registration_Category_Category_ID FROM Tournament_Registration_Category
+			WHERE Registration_Category_Event_ID = @EventID)
--- Database/Stored Procedure/Tournament Category/ReadAll_Event_Category.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[ReadAll_Category]
- @EventID INT
-
-AS
-	SELECT * FROM dbo.Tournament_Category
-	WHERE Tournament_Category_ID
-	NOT IN (SELECT Registration_Category_Category_ID FROM Tournament_Registration_Category
-			WHERE Registration_Category_Event_ID = @EventID)
